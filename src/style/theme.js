2609b9e2d1af98d38a16f35edf9051efa1349116
gui-abq <guilhermeabq@gmail.com>
2020-01-22 13:06:02 -0300

    versão-funcional

--- src/style/theme.js
+++ src/style/theme.js
@@ -3,6 +3,8 @@ import { createMuiTheme } from "@material-ui/core";
 export default createMuiTheme({
   palette: {
     primary: { main: "#ff7f50" },
-    secondary: { main: "#dcdcdc" }   
+    secondary: { main: "#dcdcdc" },
+    info: { main: "#6959cd" }
+
   }
 });
