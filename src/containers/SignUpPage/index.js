65b3c2eac950644f2de832807acbf2e72bfbc868
gui-abq <guilhermeabq@gmail.com>
2020-01-23 10:59:27 -0300

    estilizacao-pagina-cadastro

--- src/containers/SignUpPage/index.js
+++ src/containers/SignUpPage/index.js
@@ -4,7 +4,7 @@ import Button from '@material-ui/core/Button';
 import styled from 'styled-components'
 import {createUser} from '../../actions/Users'
 import {connect} from 'react-redux'
-import {logoCommentario} from '../../images/4eddit-cadastro.png'
+import logoCommentario from '../../images/logo-comentario.png'
 
 const Container = styled.div `
   display: flex;
@@ -13,12 +13,21 @@ const Container = styled.div `
   width: 100vw;
   height: 100vh;
   background-color: white ;
-  color: white;
+  color: black;
+  text-align: right;
   `
 
 const FormStyled = styled.form `
 display: flex;
   flex-direction: column;  
+`
+
+const SpanStyledCadastro = styled.span`
+  color: white;
+`
+
+const SpanStyled = styled.span`
+  color: #ff7f50;
 `
 
 class SignUpPage extends Component {
@@ -40,28 +49,28 @@ class SignUpPage extends Component {
   render() {
     return (
       <Container>
-        
         <img alt="Logo Future4" src={logoCommentario} width="300"/>
-
         <FormStyled>
             <TextField  label="Nome de usuário" name="userName" type="text" required variant="outlined"
              value={this.state.userName} onChange={this.handleInputs}/>
             <br/>
             
-            <TextField  label="email" name="email" type="email" required variant="outlined"
+            <TextField  label="Email" name="email" type="email" required variant="outlined"
              value={this.state.email} onChange={this.handleInputs}/>
             <br/>
 
-            <TextField  label="password" type="password" name="password" required variant="outlined"
+            <TextField  label="Senha" type="password" name="password" required variant="outlined"
              value={this.state.password} onChange={this.handleInputs}/>
-
+            
+            <br/>
             <Button variant="contained" color="primary" onClick={() => this.props.createUser (
               this.state.userName,this.state.email, this.state.password)}> 
-                Cadastrar 
+                <SpanStyledCadastro>Cadastrar</SpanStyledCadastro> 
             </Button>
+            <br/>
         </FormStyled>
-
         
+        <SpanStyled>Já tem uma conta?</SpanStyled>
       </Container>
     );
   }
@@ -73,4 +82,4 @@ function mapDispatchToProps (dispatch){
   })
 }
 
-export default connect (null,mapDispatchToProps)(SignUpPage);
+export default connect (null,mapDispatchToProps)(SignUpPage)
