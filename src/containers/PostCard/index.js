3d551b4f6f1313376dafbd7bf9b97ca1cbbc3acf
MateusGesualdo <mateusgcs99@gmail.com>
2020-01-23 11:05:04 -0300

    layout do Feed

--- src/containers/PostCard/index.js
+++ src/containers/PostCard/index.js
@@ -8,94 +8,108 @@ import Typography from '@material-ui/core/Typography'
 import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
 import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
 import AddCommentIcon from '@material-ui/icons/AddComment';
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import { vote, fetchDetailsPageContent } from "../../actions/Posts"
 import styled from 'styled-components'
+import Badge from '@material-ui/core/Badge';
+
 
 
 const VoteCount = styled.span`
-  color: ${props => props.color}
-
+  color: ${props => props.color};  
+  font-weight: bold;
 `
 
-
-
-
 const useStyles = makeStyles({
-    card: {
-      maxWidth: 375,
-    },
-    bullet: {
-      display: 'inline-block',
-      margin: '0 2px',
-      transform: 'scale(0.8)',
-    },
-    title: {
-      fontSize: 14,
-    },
-    pos: {
-      marginBottom: 12,
-    },
-  });
-  
+  card: {
+    width: 300,
+    display: 'flex',   
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+const Actions = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between; 
+  align-items: center;
+  background-color: aliceblue;
+`
+
 export function SimpleCard(props) {
-    const classes = useStyles();
+  const classes = useStyles();
 
   const VoteCountColor = () => {
-  if (props.post.votesCount > 0) {
-    return ('green')
-  }else if(props.post.votesCount < 0){
-    return ('red')
-  }else{
-    return ('gainsboro')
+    if (props.post.votesCount > 0) {
+      return ('blue')
+    } else if (props.post.votesCount < 0) {
+      return ('red')
+    } else {
+      return ('gainsboro')
+    }
   }
+
+  return (
+    <Card className={classes.card} color="primary">
+      <Actions>
+        <Actions>
+          <Button
+            color={props.post.userVoteDirection === 1 ? "primary" : "secondary"}
+            onClick={() => props.vote(props.post.id, 1, props.post.userVoteDirection)}
+            size="small"
+          >
+            <ArrowUpwardIcon />
+          </Button>
+
+          <VoteCount color={VoteCountColor()}>{props.post.votesCount}</VoteCount>
+
+          <Button
+            color={props.post.userVoteDirection === -1 ? "primary" : "secondary"}
+            onClick={() => props.vote(props.post.id, -1, props.post.userVoteDirection)}
+            size="small"> <ArrowDownwardIcon />
+          </Button>
+        </Actions>
+
+        <Button
+          color="secondary" size="small"
+          onClick={() => props.fetchDetailsPageContent(props.post)}
+        >
+          <Badge badgeContent={props.post.commentsNumber} color="primary">
+            <AddCommentIcon />
+          </Badge>
+        </Button>
+      </Actions>
+      <CardContent>
+
+        <Typography variant="h5" component="h2">
+          {props.post.username}
+        </Typography>
+
+        <Typography variant="body2" component="p">
+          {props.post.text}
+        </Typography>
+
+      </CardContent>
+
+    </Card>
+  );
 }
-  
-    return (
-      <Card className={classes.card} color="primary">
-        <CardContent>
-          <Typography variant="h5" component="h2">
-            {props.post.username}
-          </Typography>
 
-          <Typography variant="body2" component="p">
-            {props.post.text}
-          </Typography>
-        </CardContent>
-        <CardActions >
-          <Button
-             color={props.post.userVoteDirection === 1 ? "primary" : "secondary" } 
-             onClick={() => props.vote(props.post.id, 1, props.post.userVoteDirection)} 
-             size="small"><ArrowUpwardIcon/>
-          </Button>
+function mapDispatchToProps(dispatch) {
+  return ({
+    fetchDetailsPageContent: (post) => dispatch(fetchDetailsPageContent(post)),
+    vote: (postId, direction, userVoteDirection) => (dispatch(vote(postId, direction, userVoteDirection)))
+  })
+}
 
-          <VoteCount color={VoteCountColor ()}>{props.post.votesCount}</VoteCount>
-          
-          <Button
-            color={props.post.userVoteDirection === -1 ? "primary" : "secondary" } 
-            onClick={() => props.vote(props.post.id, -1, props.post.userVoteDirection)} 
-            size="small"> <ArrowDownwardIcon/>
-          </Button>
-
-          {props.post.commentsNumber}
-
-          <Button 
-            color="secondary" size="small" 
-            onClick={() => props.fetchDetailsPageContent (props.post)}>
-            <AddCommentIcon/>
-          </Button>
-          
-        </CardActions>
-      </Card>
-    );
-  }
-
-  function mapDispatchToProps (dispatch) {
-      return ({
-        fetchDetailsPageContent: (post) => dispatch (fetchDetailsPageContent(post)),
-        vote: (postId, direction, userVoteDirection) => (dispatch(vote(postId, direction, userVoteDirection)))
-      })
-  }
-
-export default connect (null, mapDispatchToProps)(SimpleCard)
-  
+export default connect(null, mapDispatchToProps)(SimpleCard)
--- src/containers/PostDetailsPage/index.js
+++ src/containers/PostDetailsPage/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
@@ -8,91 +8,91 @@ import Typography from '@material-ui/core/Typography'
 import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
 import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
 import TextField from '@material-ui/core/TextField';
-import {createComment, vote, voteAndUpdate} from '../../actions/Posts'
+import { createComment, vote, voteAndUpdate } from '../../actions/Posts'
 import styled from 'styled-components'
 import CommentCard from '../CommentCard'
-import {routes} from '../Router/'
-import {push} from 'connected-react-router'
+import { routes } from '../Router/'
+import { push } from 'connected-react-router'
 import AddCommentIcon from '@material-ui/icons/AddComment';
 import { makeStyles } from '@material-ui/core/styles';
 
 
-const Container = styled.div `
+const Container = styled.div`
     margin-left: 30px;
 `
 const useStyles = makeStyles({
     card: {
-      maxWidth: 375,
+        maxWidth: 375,
     },
     bullet: {
-      display: 'inline-block',
-      margin: '0 2px',
-      transform: 'scale(0.8)',
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
     },
     title: {
-      fontSize: 14,
+        fontSize: 14,
     },
     pos: {
-      marginBottom: 12,
+        marginBottom: 12,
     },
-  });
- 
+});
+
 
 
 class DetailsPage extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props)
-        
+
         this.state = {
             newComment: "",
         }
     }
 
 
-    
 
-    componentDidMount () {
 
-        if (this.props.currentPostComments.length === 0) {
-            this.props.goToFeed ()
+    componentDidMount() {
+
+        if (!this.props.currentPost) {
+            this.props.goToFeed()
         }
     }
 
     handleInputs = (event) => {
         this.setState({
-          [event.target.name]: event.target.value
+            [event.target.name]: event.target.value
         })
-      }
+    }
 
-    render () {  
-        
-          const VoteCount = styled.span`
+    render() {
+
+        const VoteCount = styled.span`
             color: ${props => props.color}
             `
-        
 
-        
+
+
 
         const VoteCountColor = () => {
-        if (this.props.currentPost.votesCount > 0) {
-            return ('green')
-        }else if(this.props.currentPost.votesCount < 0){
-            return ('red')
-        }else{
-            return ('gainsboro')
-        }
+            if (this.props.currentPost.votesCount > 0) {
+                return ('green')
+            } else if (this.props.currentPost.votesCount < 0) {
+                return ('red')
+            } else {
+                return ('gainsboro')
+            }
         }
 
-        return(
-            
+        return (
+
             <Container>
                 <Card>
                     <CardContent>
-                        
+
                         <Typography variant="h5" component="h2">
                             {this.props.currentPost.username}
                         </Typography>
-                    
+
                         <Typography variant="body2" component="p">
                             {this.props.currentPost.text}
                         </Typography>
@@ -100,62 +100,62 @@ class DetailsPage extends React.Component {
                     </CardContent>
 
                     <Button
-                        color={this.props.currentPost.userVoteDirection === 1 ? "primary" : "secondary" } 
-                        onClick={() => this.props.voteAndUpdate (this.props.currentPost, +1)} 
-                        size="small"><ArrowUpwardIcon/>
+                        color={this.props.currentPost.userVoteDirection === 1 ? "primary" : "secondary"}
+                        onClick={() => this.props.voteAndUpdate(this.props.currentPost, +1)}
+                        size="small"><ArrowUpwardIcon />
                     </Button>
 
-                    <VoteCount color={VoteCountColor ()}>{this.props.currentPost.votesCount}</VoteCount>
-                    
+                    <VoteCount color={VoteCountColor()}>{this.props.currentPost.votesCount}</VoteCount>
+
                     <Button
-                        color={this.props.currentPost.userVoteDirection === -1 ? "primary" : "secondary" } 
-                        onClick={() => this.props.voteAndUpdate (this.props.currentPost, -1)} 
-                        size="small"> <ArrowDownwardIcon/>
+                        color={this.props.currentPost.userVoteDirection === -1 ? "primary" : "secondary"}
+                        onClick={() => this.props.voteAndUpdate(this.props.currentPost, -1)}
+                        size="small"> <ArrowDownwardIcon />
                     </Button>
 
                     {this.props.currentPost.commentsNumber}
 
                     {" Comentários"}
                 </Card>
-                
+
 
                 <form>
 
-                    <TextField  
+                    <TextField
                         label="comentários" name="newComment" type="text"
-                        required variant="outlined" value={this.state.newComment} 
+                        required variant="outlined" value={this.state.newComment}
                         onChange={this.handleInputs}>
                     </TextField>
 
                     <Button variant="contained" color="primary" onClick={() => this.props.createComment(this.props.currentPost, this.state.newComment)}> Comentar </Button>
-                                
-                    {this.props.currentPostComments.map (
-                        comment => (<CommentCard comment = {comment} currentPostId = {this.props.currentPost.id}/>)
+
+                    {this.props.currentPostComments.map(
+                        comment => (<CommentCard comment={comment} currentPostId={this.props.currentPost.id} />)
                     )}
 
 
                 </form>
 
             </Container>
-            )
+        )
     }
 }
 
-function mapDispatchToProps (dispatch) {
+function mapDispatchToProps(dispatch) {
     return ({
         createComment: (currentPostId, newComment) => dispatch(createComment(currentPostId, newComment)),
         goToFeed: () => dispatch(push(routes.feed)),
-        vote: (currentPostId, direction, userVoteDirection) => dispatch (vote(currentPostId, direction, userVoteDirection)),
-        voteAndUpdate: (post, direction) => dispatch (voteAndUpdate(post, direction))
+        vote: (currentPostId, direction, userVoteDirection) => dispatch(vote(currentPostId, direction, userVoteDirection)),
+        voteAndUpdate: (post, direction) => dispatch(voteAndUpdate(post, direction))
     })
 }
 
-function mapStateToProps (state)  {
-    return ({    
+function mapStateToProps(state) {
+    return ({
         currentPost: state.postReducer.currentPost,
         currentPostComments: state.postReducer.currentPostComments
     })
 }
 
-export default connect (mapStateToProps,mapDispatchToProps) (DetailsPage)
+export default connect(mapStateToProps, mapDispatchToProps)(DetailsPage)
 
